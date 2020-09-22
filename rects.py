e3ef6713f27d9e245303bd1db62fcfc67adf72a6
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-22 14:24:07 -0400

    Cambia rects.py
    agrega un segundo rectangulo al programa rects.py en forma de tupla

--- rects.py
+++ rects.py
@@ -1,5 +1,9 @@
 """
 Representa un rectangulo con la clase Rect y lo dibuja en pantalla con la funcion pygame.draw.rect
+
+Cambio:
+  *Crea un segundo rectangulo mediante una tupla
+
 """
 import pygame
 import sys
@@ -14,12 +18,15 @@ pygame.display.set_caption('Rectangulos')
 
 white = (255, 255, 255)
 red = (115, 38, 80)
+green = (0, 255, 0)
 
-rect = pygame.Rect(100, 150, 120, 60)   # Crea el rectangulo
-rect.center = (width // 2, height // 2) # Coloca el rectangulo en el centro de la pantalla 
+rect = pygame.Rect(100, 150, 120, 60)  
+rect.center = (width // 2, height // 2)
 
 print(rect.x)
 print(rect.y)
+
+rect2 = (100, 100, 80, 40) # segunda forma de crear un rectangulo
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +36,7 @@ while True:
 
     surface.fill(white)
 
-    pygame.draw.rect(surface, red, rect) # dibuja el rectangulo en la pantalla
+    pygame.draw.rect(surface, red, rect)
+    pygame.draw.rect(surface, green, rect2)
 
     pygame.display.update()
