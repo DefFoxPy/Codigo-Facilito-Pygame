38512d59fa75c67bfd53d913b5b53694aaeecad2
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-09-29 09:04:00 -0400

    Update circle_collition.py
    colisión entre dos circulos por medio de la distancia de la suma de los dos radios

--- introduccion/circle_collition.py
+++ introduccion/circle_collition.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 
 pygame.init()
 
@@ -9,16 +10,17 @@ height = 500
 screen = pygame.display.set_mode((width, height))
 
 white = (255, 255, 255)
+black = (0, 0, 0)
+
+font = pygame.font.Font(None, 40)
 
 image1 = pygame.image.load('./images/medium_circle.png')
 rect1 = image1.get_rect()
 rect1.center = (width // 2, height // 2)
 
-surface1 = pygame.Surface((rect1.width, rect1.height), pygame.SRCALPHA)
-surface1.fill((0, 0, 0, 50))
-rect2 = surface1.get_rect()
+image2 = pygame.image.load('./images/medium_circle.png')
+rect2 = image2.get_rect()
 rect2.center = rect1.center
-
 
 while True:
 	for event in pygame.event.get():
@@ -26,7 +28,23 @@ while True:
 			pygame.quit()
 			sys.exit()
 
+	message = ''
+
+	rect1.center = pygame.mouse.get_pos()
+
+	# determina la distancia entre los centros de los circulos
+	dist = math.hypot(rect1.x - rect2.x, rect1.y - rect2.y)
+
+	# distancia menor a la suma de los radios
+	if dist < (64 + 64): 
+		message = 'Existe una colisión'
+
+	text = font.render(message, True, black)
+	text_rect = text.get_rect()
+	text_rect.midtop = (width // 2, 50)
+
 	screen.fill(white)
 	screen.blit(image1, rect1)
-	screen.blit(surface1, rect2)
+	screen.blit(image2, rect2)
+	screen.blit(text, text_rect)
 	pygame.display.update()
