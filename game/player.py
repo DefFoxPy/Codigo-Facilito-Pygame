4c39f7054652f1805d0316c7f2ed1332f65b3cd7
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2020-10-15 09:01:07 -0400

    agrega la animacion

--- game/player.py
+++ game/player.py
@@ -8,7 +8,12 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self, left, bottom, dir_images):
 		pygame.sprite.Sprite.__init__(self)
 
-		self.image = pygame.image.load(os.path.join(dir_images, 'player1.png'))
+		self.images = (
+			pygame.image.load(os.path.join(dir_images, 'player1.png')),
+			pygame.image.load(os.path.join(dir_images, 'jump.png'))
+		)
+
+		self.image = self.images[0]
 
 		self.rect = self.image.get_rect()
 		self.rect.left = left
@@ -33,6 +38,7 @@ class Player(pygame.sprite.Sprite):
 		self.pos_y = wall.rect.top
 		self.vel_y  = 0
 		self.can_jump = True
+		self.image = self.images[0]
 
 	def validate_platform(self, platform):
 		result = pygame.sprite.collide_rect(self, platform)
@@ -41,11 +47,13 @@ class Player(pygame.sprite.Sprite):
 			self.vel_y = 0
 			self.pos_y = platform.rect.top
 			self.can_jump = True
+			self.image = self.images[0]
 
 	def jump(self):
 		if self.can_jump:
 			self.vel_y = -23
 			self.can_jump = False
+			self.image = self.images[1] # cambio de imagen a saltar
 
 	def update_pos(self):
 		self.vel_y += PLAYER_GRAV
